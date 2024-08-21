678d41e5f445979b0c6baf80c33cb836d075fdb7
Sam Arbid <samaa2@kth.se>
2024-08-21 18:47:36 +0200

    update script

---
+++ install_local_packages.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2024 KTH.
+#
+# KTH-RDM is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+# Quit on errors
+set -uo errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+
+echo "Installing dev packages ..."
+invenio-cli packages install ~/INVENIO/issues/invenio-app-rdm/ ~/INVENIO/issues/invenio-users-resources/ ~/INVENIO/issues/invenio-communities/ ~/INVENIO/issues/invenio-rdm-records/ ~/INVENIO/issues/invenio-records-resources/ ~/INVENIO/issues/invenio-accounts/
