0af7638176c8c0b4257439ae86f54e38e194dc96
Sam Arbid <samaa2@kth.se>
2026-02-18 17:04:25 +0100

    feat: update permission policy
    * check against role.id instead of role.name
    * Implement CommunitiesPermissionPolicy to restrict
      community creation to specific roles.
    * Introduce CommunityCreator class for role-based
      permissions management.

---
+++ site/latest_build/permissions.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 KTH Royal Institute of Technology Sweden
+#
+# invenio-config-kth is free software, you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file details.
+
+"""Permission configurations."""
+
+# See https://github.com/inveniosoftware/invenio-rdm-records/commit/3253af28cb1ab67c920fe53bc237d2fab5344d82
+
+from flask import current_app
+from flask_principal import RoleNeed
+from invenio_administration.generators import Administration
+from invenio_communities.permissions import CommunityPermissionPolicy
+from invenio_records_permissions.generators import Disable, Generator, SystemProcess
+from invenio_accounts.proxies import current_datastore
+
+
+class CommunityCreator(Generator):
+    """Allows users with the community-creator role."""
+
+    def needs(self, **kwargs):
+        configured = current_app.config.get(
+            "CONFIG_COMMUNITY_CREATOR_ROLE", "community-creator"
+        )
+
+        role = current_datastore.find_role(configured)
+
+        if role is None:
+            return [RoleNeed(configured)]
+
+        return [RoleNeed(role.id)]
+
+
+class CommunitiesPermissionPolicy(CommunityPermissionPolicy):
+    """Communities permission policy of KTH.
+
+    This will enable community creator and admins only to create communities.
+
+    deprecating:  https://pypi.org/project/invenio-config-kth/ for versions <= V11.
+    """
+
+    can_create = [CommunityCreator(), Administration(), SystemProcess()]
+
+    # Restrict unsafe actions to admins only (remove CommunityOwners)
+    can_delete = [Administration(), SystemProcess()]
+    can_rename = [Administration(), SystemProcess()]
+
+    can_include_directly = [Disable()]
