799c6d7265634a6c2102c66423eb1e6de7f7d3e5
Sam Arbid <samaa2@kth.se>
2026-06-09 11:05:49 +0200

    script: update seed users

--- scripts/dev/seed_users.sh
+++ scripts/dev/seed_users.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-# Number of users to create (default: 10)
-NUM_USERS=${1:-10}
 
-echo "Creating $NUM_USERS users..."
+# run like ./scripts/dev/seed_users.sh 10 20 
+# first argument is the number of users to create, second argument is the starting index for user numbering
+COUNT=${1:-10}
+START=${2:-1}
+PASSWORD="123456"
 
-# Create the first fixed user
-echo "Creating first fixed user: test@test.com"
-uv run invenio users create test@test.com --password 123456 -c -a -u test
+END=$((START + COUNT - 1))
 
-# Create users 1..(NUM_USERS-1)
-for ((i=1; i<NUM_USERS; i++)); do
-    EMAIL="test$i@test$i.com"
-    USERNAME="test$i"
-    echo "Creating user: $EMAIL  username: $USERNAME"
-    uv run invenio users create "$EMAIL" --password 123456 -c -a -u "$USERNAME"
+echo "Creating $COUNT users from $START to $END..."
+
+for ((i=START; i<=END; i++)); do
+    EMAIL="test${i}@test${i}.com"
+    USERNAME="test${i}"
+
+    echo "Creating user: $EMAIL username: $USERNAME"
+
+    uv run invenio users create "$EMAIL" \
+        --password "$PASSWORD" \
+        -c -a \
+        -u "$USERNAME" \
+        || echo "Skipping existing user: $EMAIL"
 done
 
 echo "Done!"
